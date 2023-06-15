4c6866c82cc073022b48c60cfc86c9ae0961227a
Roman <roman.rostock@gmail.com>
2023-06-15 14:05:37 +0200

    corrected validation

--- frontside/src/validations/PartnerserviceValidation.js
+++ frontside/src/validations/PartnerserviceValidation.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
- export const PartnerserviceSchema = Yup.object().shape({
+export const PartnerserviceSchema = Yup.object().shape({
     title: Yup.string().required("Bitte geben Sie den Titel ein"),
-    content:Yup.string().required("Bitte geben Sie einen Servicenamen ein"),
+    content: Yup.array().min( 1,"Bitte geben Sie die Servicenamen ein"),
 });
