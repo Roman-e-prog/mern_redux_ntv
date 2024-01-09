7baa85e2916c3bc70a56c4ed99f3f48014eb217d
Roman-e-prog <roman.rostock@googlemail.com>
2024-01-09 05:58:50 +0100

    added db connectionConfig

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
-
+const connectionOptions = {
+    dbName: 'Mern_redux',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // other options...
+  };
 const connectDb = async ()=>{
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        mongoose.set("strictQuery", false);
+        const conn = await mongoose.connect(process.env.MONGO_URL, connectionOptions);
         console.log(`MongoDB connected : ${conn.connection.host}`.cyan.underline);
     } catch(error){
         console.log(error);
